5181241e449b58c7565e94145b1a646d68caa4ca
lmitchell4 <lmitchell4@gmail.com>
2017-04-17 06:17:13 -0700

    Further cleanup

--- js/script.js
+++ js/script.js
@@ -106,9 +106,7 @@ function initMap() {
 
   // Create the map:
   map = new google.maps.Map(document.getElementById("map"), {
-    // center: {lat: 40.7413549, lng: -73.9980244},
-    // center: {lat: 41.8781, lng: -87.6298},
-    center: {lat: 41.8622646, lng: -87.61663820000001},
+    center: {lat: 41.8781, lng: -87.6298},
     zoom: 13,
     // styles: styles,
     mapTypeControl: false
@@ -435,20 +433,21 @@ ko.applyBindings(myViewModel);
 
 
 
-document.addEventListener('DOMContentLoaded', function () {  
-  if(document.querySelectorAll('#map').length > 0) {
-    if (document.querySelector('html').lang)
-      lang = document.querySelector('html').lang;
-    else
-      lang = 'en';
-
-    var js_file = document.createElement('script');
-    js_file.type = 'text/javascript';
+document.addEventListener("DOMContentLoaded", function () {  
+  if(document.querySelectorAll("#map").length > 0) {
+    if(document.querySelector("html").lang) {
+      lang = document.querySelector("html").lang;
+    } else {
+      lang = "en";
+    }
+    
+    var js_file = document.createElement("script");
+    js_file.type = "text/javascript";
     js_file.async = true;
     js_file.defer = true;
-    js_file.src = 'https://maps.googleapis.com/maps/api/js?libraries=places,geometry,drawing&key=' + 
-                    maps_api_key + '&callback=initMap&language=' + lang;
-    document.getElementsByTagName('head')[0].appendChild(js_file);
+    js_file.src = "https://maps.googleapis.com/maps/api/js?libraries=places&key=" + 
+                    maps_api_key + "&callback=initMap&language=" + lang;
+    document.getElementsByTagName("head")[0].appendChild(js_file);
     
   }
 });
